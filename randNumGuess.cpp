69066739d17186431ae5d1e0b18a16402b76bfbf
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2023-10-18 21:44:36 +0000

    updated randNumGuess.cpp

--- randNumGuess.cpp
+++ randNumGuess.cpp
@@ -9,13 +9,13 @@
 
 int main() {
     // declare variables
-    int someRandomNumber, userGuess;
+    int randomAnswer, userGuess;
 
     // initialize random seed
     srand((unsigned)time(NULL));
 
     // generate a random number between 0 and 9
-     someRandomNumber = rand_r() % 9 + 0;
+    randomAnswer = rand_r() % 9 + 0;
 
     // get the user guess
     std ::cout << "An integer has been chosen from 0 to 9." << std::endl;
@@ -23,11 +23,11 @@ int main() {
     std::cin >> userGuess;
 
     // if the user gets the number right, tell them they were right
-    if (userGuess == someRandomNumber) {
+    if (userGuess == randomAnswer) {
         std ::cout << "Your guess was right!" << std::endl;
     } else {
         // otherwise, tell them they were wrong
-        std ::cout << "Your guess was wrong! The answer was " <<
-        someRandomNumber << "." << std::endl;
+        std ::cout << "Your guess was wrong! The answer was " << 
+        randomAnswer << "." << std::endl;
     }
 }
